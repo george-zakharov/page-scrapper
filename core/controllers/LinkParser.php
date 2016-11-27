bd0e8625bc91656e59f235e41c073fdacf42fbd2
George Zakharov <george_zakharov@mail.ru>
2016-11-27 21:11:35 +0300

    Update file structure

---
+++ core/controllers/LinkParser.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Link parser.
+ * Parse links from the URL given.
+ *
+ * @package: pageScrapper
+ * @version: 1.0
+ * @author: wildwind | George Zakharov <george_zakharov@mail.ru>
+ */
+
+namespace core\controllers;
+
+
+class LinkParser
+{
+
+}
