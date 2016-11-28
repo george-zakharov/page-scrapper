5588c1790faf84c08f4bd5124b03fd085fe9f150
George Zakharov <george_zakharov@mail.ru>
2016-11-29 00:42:50 +0300

    Add link parser

---
+++ core/controllers/ParserAbstract.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Parser abstraction.
+ * Parse data from the URL given.
+ *
+ * @package: pageScrapper
+ * @version: 1.0
+ * @author: wildwind | George Zakharov <george_zakharov@mail.ru>
+ */
+
+namespace core\controllers;
+
+
+abstract class ParserAbstract
+{
+    public static $resultData = [];
+
+    /**
+     * @param mixed $url
+     */
+    public static function parseDataFromUrl($url)
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public static function getResultData()
+    {
+        return self::$resultData;
+    }
+}
